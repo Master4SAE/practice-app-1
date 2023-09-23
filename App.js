97d58245c82267f7a3ad035fc33f75ee36d1fa1b
sohaibebrahimi <ebrahimi_sa@icloud.com>
2023-09-23 12:18:21 +0300

    comit

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+const App =()=> {
   return (
-    <View style="">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <ScrollView>
-        <Text> Some text </Text>
-        <View>
-          <Text>some more text</Text>
-
-          
-        </View>
-
-        <TextInput defaultValue='Type here'/>
-
-    </ScrollView>
+    <View>
+      <Text>hello</Text>
     </View>
-  );
+  )
 }
+
 
 
